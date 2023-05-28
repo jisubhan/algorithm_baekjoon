7e67ce37a3857628d120780a67ea40a03c7c501e
Ji sub Han <31399124+jisubhan@users.noreply.github.com>
2023-05-28 22:43:21 +0900

    [unrated] Title: 기사단원의 무기, Time: 3.74 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.py
@@ -0,0 +1,13 @@
+def cf(n):
+    a = []
+    for i in range(1, int(n**0.5) + 1):
+        if n% i ==0:
+            a.append(n//i)
+            a.append(i)
+    return len(set(a))
+
+def solution(number, limit, power):
+    return sum([cf(i) if cf(i) <=limit else power for i in range(1,number+1)])
+    
+    
+
